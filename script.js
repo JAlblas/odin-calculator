729ec67a4f5441d105c6d3954b7a78c9b6812db5
Jasper Alblas <just_jasper@hotmail.com>
2024-05-29 19:39:51 +0200

    Adds basic js calculator functionality
    Basic calculation methods added and project structure

---
+++ script.js
@@ -0,0 +1,27 @@
+let add = (a, b) => a + b;
+let substract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+operator = "";
+firstNumber = 0;
+secondNumber = 0;
+
+let operate = (operator, num1, num2) => {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return substract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+        default:
+        // code block
+    }
+}
