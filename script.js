440ecddd6ce1845035a68fa340c6a9cf1d47a907
Jasper Alblas <just_jasper@hotmail.com>
2024-05-31 09:50:42 +0200

    Number buttons
    Adds number buttons logic with updating of display

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+let displayText = document.querySelector('#display p');
+
+let displayValue = 0;
+
+let updateDisplay = (value) => {
+    displayText.textContent = value;
+}
+
+/*
+let five = document.querySelector('#five');
+five.addEventListener("click", () => {
+    updateDisplay('5');
+})
+
+let six = document.querySelector('#six');
+six.addEventListener("click", () => {
+    updateDisplay('6');
+})
+*/
+
+numericButtons = document.querySelectorAll('.number');
+numericButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if (displayValue == 0) {
+            displayValue = button.textContent;
+        }
+        else {
+            displayValue += button.textContent;
+
+        }
+        updateDisplay(displayValue);
+    })
+});
+
+
+
+let clear = document.querySelector('#clear');
+clear.addEventListener("click", () => {
+    updateDisplay('');
+})
+
+
 let add = (a, b) => a + b;
 let substract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
@@ -25,3 +67,4 @@ let operate = (operator, num1, num2) => {
         // code block
     }
 }
+
